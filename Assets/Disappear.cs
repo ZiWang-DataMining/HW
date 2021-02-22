42c35773be79ae6880b25872b5dc280208730fef
Zack Wang <2010zihao@gmail.com>
2021-02-22 02:45:42 -0800

    tag name update

---
+++ Assets/Disappear.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Disappear : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag.Equals("fishball"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
